1d08c175f655fdeae64a12ec868527acc1e2a39c
Christopher Kelter <christopher.kelter@student.uibk.ac.at>
2026-04-06 12:54:50 +0200

    init project setup

---
+++ src/main/java/at/oeh/uni/innsbruck/stadtrad/examValidation/ExamValidationApplication.java
@@ -0,0 +1,13 @@
+package at.oeh.uni.innsbruck.stadtrad.examValidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamValidationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/oeh/uni/innsbruck/stadtrad/examValidation/ExamValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package at.oeh.uni.innsbruck.stadtrad.examValidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
