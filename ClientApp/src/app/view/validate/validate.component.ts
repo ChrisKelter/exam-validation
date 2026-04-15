3032720fa40e32877ac3fc9c1424077493cb3548
Christopher Kelter <christopher.kelter@student.uibk.ac.at>
2026-04-15 13:36:05 +0200

    add ui

--- ClientApp/src/app/view/validate/validate.component.ts
+++ ClientApp/src/app/view/validate/validate.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import {Card} from "primeng/card";
+import {FileUpload} from "primeng/fileupload";
+import {InputText} from "primeng/inputtext";
+import {Button, ButtonDirective} from "primeng/button";
+import {Checkbox} from "primeng/checkbox";
 
 @Component({
   selector: 'app-validate',
   standalone: true,
-  imports: [],
+  imports: [
+    Card,
+    FileUpload,
+    InputText,
+    ButtonDirective,
+    Button,
+    Checkbox
+  ],
   templateUrl: './validate.component.html',
   styleUrl: './validate.component.css'
 })
